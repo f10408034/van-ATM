1ac7988ad20f6cfdedcbf9be263c930c0f6d122a
lm <f10408034@gmail.com>
2022-03-10 11:16:06 +0800

    remember username with checkbox 2022/03/10

--- app/src/main/java/com/vangood/atm/FirstFragment.kt
+++ app/src/main/java/com/vangood/atm/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.vangood.atm
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,7 +14,7 @@ import com.vangood.atm.databinding.FragmentFirstBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
+    var remember = false
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,13 +33,34 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val pref = requireContext().getSharedPreferences("atm", Context.MODE_PRIVATE)
+        
+        val checked = pref.getBoolean("rem_username", false)
+        binding.cbRemember.isChecked = checked
+        binding.cbRemember.setOnCheckedChangeListener { compoundButton, checked ->
+            remember = checked
+            pref.edit().putBoolean("rem_username", remember).apply()
+            if (!checked) {
+                pref.edit().putString("USER", "").apply()
+            }
+        }
+        val prefuser = pref.getString("USER","")
+        if (prefuser != "") binding.edUsername.setText(prefuser)
 
         binding.buttonFirst.setOnClickListener {
-
             //Login stuff
-            if (binding.edUsername.text.toString() == "jack" &&
-                    binding.edPassword.text.toString() == "1234"){
-                        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val username = binding.edUsername.text.toString()
+            val password = binding.edPassword.text.toString()
+            if (username == "jack" && password == "1234"){
+                //save username to preferences
+                //val pref = requireContext().getSharedPreferences("atm", Context.MODE_PRIVATE)
+                if (remember) {
+                    pref.edit()
+                        .putString("USER", username)
+                        .putInt("LEVEL", 3)
+                        .apply() //.commit()
+                }
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
             } else {
                 //error
                 AlertDialog.Builder(requireContext())
